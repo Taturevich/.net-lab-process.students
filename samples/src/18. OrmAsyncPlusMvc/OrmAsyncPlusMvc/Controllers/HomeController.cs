3ca8a833b0ac526e9710a072326ef3bcac3488cf
Ivan <ivan_taturevich@epam.com>
2020-02-04 23:55:53 +0300

    added additional samples for async showcase

--- samples/src/18. OrmAsyncPlusMvc/OrmAsyncPlusMvc/Controllers/HomeController.cs
+++ samples/src/18. OrmAsyncPlusMvc/OrmAsyncPlusMvc/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading;
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using EF_Task;
@@ -7,6 +11,11 @@ namespace AsyncVsSyncExample.Controllers
 {
     public class HomeController : Controller
     {
+        private static HttpClient _httpClient = new HttpClient
+        {
+            BaseAddress = new System.Uri("https://www.google.com/")
+        };
+
         private readonly IProductCategoryService _productCategoryService;
 
         public HomeController(IProductCategoryService productCategoryService)
@@ -48,6 +57,33 @@ namespace AsyncVsSyncExample.Controllers
         {
             ViewBag.Message = "Your application description page.";
             return View();
+        }
+
+        public async Task<ActionResult> NetworkCallAsync()
+        {
+            var response = await _httpClient.GetAsync("");
+            var result = await response.Content.ReadAsStringAsync();
+            return Content(result);
+        }
+
+        public ActionResult NetworkCall()
+        {
+            var webRequest = WebRequest.Create("https://www.google.com/");
+            var response = webRequest.GetResponse();
+            var memoryStream = new MemoryStream();
+            using (var stream = response.GetResponseStream())
+            {
+                stream.CopyTo(memoryStream);
+            }
+            var result = Encoding.UTF8.GetString(memoryStream.ToArray());
+
+            return Content(result);
+        }
+
+        public async Task<ActionResult> AsyncCategory()
+        {
+            var productInfo = await _productCategoryService.GetProductInfoForCategoryAsync(3);
+            return Json(productInfo, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult Contact()
