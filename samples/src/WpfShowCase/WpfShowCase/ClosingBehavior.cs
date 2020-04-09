dad66e1686f399382dcc70346d3ad7d0b41ec3b5
Ivan_Taturevich <ivan_taturevich@epam.com>
2020-04-09 19:59:14 +0300

    added WPF sample project after online classes

---
+++ samples/src/WpfShowCase/WpfShowCase/ClosingBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace WpfShowCase
+{
+    public class ClosingBehavior : Behavior<Window>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Closing += AssociatedObject_Closing;
+        }
+
+        private void AssociatedObject_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var result = MessageBox.Show("Are you sure?", "Confirmation", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+                AssociatedObject.Height = UserSize;
+            }
+        }
+
+        public static readonly DependencyProperty UserSizeProperty = DependencyProperty.Register("UserSize", typeof(double), typeof(ClosingBehavior));
+
+        public double UserSize
+        {
+            get { return (double)GetValue(UserSizeProperty); }
+            set { SetValue(UserSizeProperty, value); }
+        }
+    }
+}
---
+++ samples/src/WpfShowCase/WpfShowCase/IsColor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace WpfShowCase
+{
+    public static class IsColor
+    {
+        public static readonly DependencyProperty IsBubbleSourceProperty = DependencyProperty.RegisterAttached(
+            "IsColorRed",
+            typeof(bool),
+            typeof(IsColor),
+            new PropertyMetadata(null));
+
+        public static void SetIsColorRed(UIElement element, bool value)
+        {
+            element.SetValue(IsBubbleSourceProperty, value);
+        }
+
+        public static bool GetIsColorRed(UIElement element)
+        {
+            return (bool)element.GetValue(IsBubbleSourceProperty);
+        }
+    }
+}
